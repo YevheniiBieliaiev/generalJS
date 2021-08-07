8b7aa3d4f85093dfc8e265d776c0665e1aef035d
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2021-08-07 13:16:40 +0300

    changes

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 console.log("First task:");
 
 const student = {
-  firstName: "Jim",
-  lastName: "Beam",
+  firstName: "",
+  lastName: "",
   averageScore: 0,
   getFullName() {
     return `${this.firstName} ${this.lastName}`;
@@ -14,20 +14,22 @@ const student = {
   }
 }
 
-function Aspirant() {
+function Aspirant(firstName, lastName, diserStatus) {
   this.__proto__ = student;
+  this.firstName = firstName;
+  this.lastName = lastName;
   this.dissertationTopic;
-  this.isDissertationComplete;
+  this.isDissertationComplete = diserStatus;
   this.isGrantHolder = function () {
     return (this.averageScore >= 4.5 && this.isDissertationComplete);
   }
 }
 
-const aspirant = new Aspirant();
+const aspirant = new Aspirant("Jim", "Beam", false);
 
 student.averageScore = 4.8
+console.log(aspirant.getFullName());
 console.log(student.isGrantHolder()); // true
-aspirant.isDissertationComplete = false;
 console.log(aspirant.isGrantHolder()); // false
 
 
@@ -43,33 +45,33 @@ for (let key in aspirant) {
 // 2) 
 console.log("Second task:");
 
+
+
 class Plane {
-  constructor(name) {
+  constructor(name, isFlying) {
     this.name = name;
-    this.isFlying = false;
-    this.takeOff = function () {
-      return this.isFlying = true;
-    }
-    this.land = function () {
-      if (this.isFlying) {
-        return this.isFlying = false;
-      }
-    }
+    this.isFlying = isFlying;
   }
+  takeOff() {
+    return this.isFlying = true;
+  };
+  land() {
+    return this.isFlying = false;
+  };
 }
 
 const airport = {
   planes: [],
   getFlyingPlanes() {
-    return this.planes.filter(item => item.isFlying === true);
+    return this.planes.filter(item => item.isFlying);
   }
 }
 
 
-let plane1 = new Plane("First");
-let plane2 = new Plane("Killer");
-let plane3 = new Plane("Airbus");
-let plane4 = new Plane("Boeing");
+let plane1 = new Plane("First", true);
+let plane2 = new Plane("Killer", false);
+let plane3 = new Plane("Airbus", false);
+let plane4 = new Plane("Boeing", false);
 
 plane2.takeOff();
 plane2.land();
